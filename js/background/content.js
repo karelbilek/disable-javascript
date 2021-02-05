620902942bf5590730ba16a8ee4495cd3ad53f99
Karel Bilek <karel.bilek@brank.as>
2021-02-05 19:58:56 +0700

    Fix iframe

--- js/background/content.js
+++ js/background/content.js
@@ -1,5 +1,10 @@
 'use strict';
 
+function htmlDecode(input) {
+  var doc = new DOMParser().parseFromString(input, "text/html");
+  return doc.documentElement.textContent;
+}
+
 (function() {
   var tags = document.getElementsByTagName('noscript');
 
@@ -9,7 +14,7 @@
       var a = tags[i].getAttributeNames();
 
       // Copy over <noscript> content.
-      newDiv.innerHTML = tags[i].innerHTML;
+      newDiv.innerHTML = htmlDecode(tags[i].innerHTML);
 
       // Copy over <noscript> attributes.
       for (var k = 0; k < a.length; k++) {
